1a5c5fe9370fa5b67d82a88e2763bc36f7683aa7
nmathlouthi2023 <methlouthi.nebyl@gmail>
2023-08-15 01:55:09 +0200

    Importing dependencies using Spring Initializr

---
+++ src/test/java/com/db/sqltomongo/DbSqlToMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.sqltomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSqlToMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
